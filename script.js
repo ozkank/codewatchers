1e4eda645c244028c1167ff426d526f233f1e4bf
Ahmet Şenlik <ahmet.senlik@sabancidx.com>
2023-12-22 01:13:11 +0300

    update timer

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
-let symbols = ['🌟', '🍎', '🐱', '🌈', '🚀', '🎈', '🍕', '🐶'];
+let symbols = ['🌟', '🍎', '🐱', '🌈', '🚀', '🎈', '🍕', '🐶','🌟', '🍎', '🐱', '🌈', '🚀', '🎈', '🍕', '🐶','🎈', '🍕', '🐶'];
 let cards = [];
 let grid = document.getElementById('grid');
 let flippedCards = [];
 let matches = 0;
-let timer = 10;
-let timerInterval;
+let currentLevel = 0;
+let cardCounts = [4];
+let countdownInterval;
+let timerSeconds = 0;
 
 function startGame() {
     const username = document.getElementById('username').value;
@@ -13,65 +15,50 @@ function startGame() {
         return;
     }
 
-    // Hide login container
     document.getElementById('login-container').style.display = 'none';
-
-    // Display game container
     document.getElementById('game-container').style.display = 'flex';
 
-    // Reset the game
-    resetGame();
+    currentLevel = 0;
 
-    // Shuffle the cards
-    let cards = [...symbols, ...symbols];
-    cards.sort(() => Math.random() - 0.5);
+    const countdownElement = document.getElementById('countdown');
 
-    // Create cards and append them to the grid
-    cards.forEach((symbol, index) => {
+    // Remove the "let" keyword here to use the global variable
+    countdownInterval = setInterval(() => {
+        timerSeconds++
+        countdownElement.textContent = timerSeconds;
+        
+    }, 1000);
+    startLevel();
+}
+function startLevel() {
+    currentLevel++;
+
+    let currentCardCount = cardCounts[currentLevel - 1];
+    let levelSymbols = symbols.slice(0, currentCardCount / 2);
+    let levelCards = [...levelSymbols, ...levelSymbols];
+    levelCards.sort(() => Math.random() - 0.5);
+
+    const columns = Math.sqrt(currentCardCount);
+    grid.style.gridTemplateColumns = `repeat(${columns}, 1fr)`;
+
+    levelCards.forEach((symbol, index) => {
         const card = document.createElement('div');
         card.classList.add('card');
         card.dataset.symbol = symbol;
         card.addEventListener('click', flipCard);
         grid.appendChild(card);
     });
-
-    // Start the timer
-    timerInterval = setInterval(updateTimer, 1000);
 }
 
-function resetGame() {
-    // Close all open cards
-    const openCards = document.querySelectorAll('.card.open');
-    openCards.forEach(card => {
-        card.classList.remove('open');
-        card.textContent = '';
-    });
-
-    // Reset variables
-    flippedCards = [];
-    matches = 0;
-    timer = 10;
-
-    // Update the timer display
-    document.getElementById('timer').textContent = `Time: ${timer} seconds`;
-
-    // Clear the scoreboard
-    document.getElementById('scoreboard').textContent = '';
-
-    // Restart the timer
-    clearInterval(timerInterval);
-    timerInterval = setInterval(updateTimer, 1000);
-}
 
 function flipCard() {
-    if (timer === 0 || flippedCards.length >= 2) {
-        // If time is over or two cards are already flipped, return early
+    if (flippedCards.length >= 2) {
         return;
     }
 
     const card = this;
     card.classList.add('open');
-    card.textContent = card.dataset.symbol; // Show the symbol
+    card.textContent = card.dataset.symbol;
     flippedCards.push(card);
 
     if (flippedCards.length === 2) {
@@ -83,19 +70,19 @@ function checkMatch() {
     const [card1, card2] = flippedCards;
 
     if (card1.dataset.symbol === card2.dataset.symbol) {
-        // Cards match, remove them from the page
         card1.style.visibility = 'hidden';
         card2.style.visibility = 'hidden';
         flippedCards = [];
         matches++;
 
-        if (matches === symbols.length) {
-            // All matches made, display the scoreboard
-            clearInterval(timerInterval);
-            displayScore();
+        if (matches === cardCounts[currentLevel - 1] / 2) {
+            if (currentLevel < cardCounts.length) {
+                startLevel();
+            } else {
+                displayScore();
+            }
         }
     } else {
-        // Cards do not match, flip them back and hide the symbols
         card1.classList.remove('open');
         card2.classList.remove('open');
         card1.textContent = '';
@@ -104,21 +91,8 @@ function checkMatch() {
     }
 }
 
-function updateTimer() {
-    timer--; // Decrement the timer
-
-    // Ensure the timer doesn't go below zero
-    timer = Math.max(0, timer);
-
-    // Display the timer value
-    document.getElementById('timer').textContent = `Time: ${timer} seconds`;
-
-    if (timer === 0 && matches !== symbols.length) {
-        clearInterval(timerInterval);
-        document.getElementById('scoreboard').textContent = `Game over! Your score: ${matches} pairs.`;
-    }
-}
-
 function displayScore() {
-    document.getElementById('scoreboard').textContent = `Congratulations! You completed the game in ${10 - timer} seconds, ${document.getElementById('username').value}! Your score: ${matches} pairs.`;
+    clearInterval(countdownInterval); 
+    const message = 'Congratulations! You completed all levels, ' + document.getElementById('username').value + '!\nYour final score: ' + timerSeconds + '.';
+    document.getElementById('scoreboard').textContent = message;
 }
