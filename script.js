01e8d7de2263eb4ebbf3737dfeed6f1af5033d3e
Ahmet Şenlik <ahmet.senlik@sabancidx.com>
2023-12-22 05:54:06 +0300

    add scoreboard

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ let currentLevel = 0;
 let cardCounts = [2,4,6];
 let countdownInterval;
 let timerSeconds = 0;
+let bestScoreAchieved = false;
 let selectedCardType;
 const congrats = document.querySelector("#congratsSection");
 function startGame() {
@@ -101,13 +102,52 @@ function checkMatch() {
     }
 }
 
-function winGame() {
-    clearInterval(countdownInterval); 
+async function winGame() {
+    clearInterval(countdownInterval);
 
-    document.getElementById('canvas').style.display = 'block';
+  
+    const username = document.getElementById('username').value;
+  
+    try {
+        const lowestScoresResponse = await fetch('http://localhost:3000/getLowestScore');
+        const lowestScores = await lowestScoresResponse.json();
 
-    const message = 'Congratulations! You completed all levels, ' + document.getElementById('username').value + '!\nYour final score: ' + timerSeconds + '.';
-    document.getElementById('scoreboard').textContent = message;
+        // Check if the new score is among the lowest scores
+        const isNewLowestScore = lowestScores.some(score => timerSeconds < score.score);
+
+        if (isNewLowestScore) {
+            bestScoreAchieved = true;
+            document.getElementById('canvas').style.display = 'block';
+        }
+
+        await fetch('http://localhost:3000/saveScore', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ username, score: timerSeconds }),
+            });
+
+        const message = (bestScoreAchieved) => {
+            if (bestScoreAchieved) {
+                return `Congratulations! You completed all levels and achieved the best score, ${username}! \nYour score: ${timerSeconds}.`;
+            } else {
+                return `Congratulations! You completed all levels, ${username}!\nYour score: ${timerSeconds}.`;
+            }
+        };
+             
+        
+        const congratulatoryMessage = message(bestScoreAchieved);
+        document.getElementById('scoreboard').textContent = congratulatoryMessage;
+
+        
+        
+
+    } catch (error) {
+        console.error('Error saving score or getting lowest score:', error);
+    }
+
+    
 }
 
   
@@ -120,3 +160,39 @@ function winGame() {
     return color;
   }
   
+  document.addEventListener('DOMContentLoaded', () => {
+    getScores();
+});
+
+async function getScores() {
+    try {
+        
+        const response = await fetch('http://localhost:3000/getScores');
+        const scores = await response.json();
+
+        const scoreList = document.getElementById('score-list');
+
+        scores.forEach((score, index) => {
+            const scoreItem = document.createElement('tr');
+
+            const rankCell = document.createElement('td');
+            rankCell.textContent = index + 1;
+
+            const usernameCell = document.createElement('td');
+            usernameCell.classList.add('username');
+            usernameCell.textContent = score.username;
+
+            const scoreCell = document.createElement('td');
+            scoreCell.classList.add('score');
+            scoreCell.textContent = score.score;
+
+            scoreItem.appendChild(rankCell);
+            scoreItem.appendChild(usernameCell);
+            scoreItem.appendChild(scoreCell);
+
+            scoreList.appendChild(scoreItem);
+        });
+    } catch (error) {
+        console.error('Error getting scores:', error);
+    }
+}
